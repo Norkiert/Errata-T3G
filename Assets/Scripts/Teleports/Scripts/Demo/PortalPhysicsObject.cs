b61eaf0efada2178fa9e5ce26aacedb05620be86
bagnohash <klocseb15@gmail.com>
2021-11-30 15:35:18 +0100

    Portale

---
+++ Assets/Scripts/Teleports/Scripts/Demo/PortalPhysicsObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody))]
+public class PortalPhysicsObject : PortalTraveller {
+
+    public float force = 10;
+    new Rigidbody rigidbody;
+    public Color[] colors;
+    static int i;
+
+    void Awake () {
+        rigidbody = GetComponent<Rigidbody> ();
+        graphicsObject.GetComponent<MeshRenderer> ().material.color = colors[i];
+        i++;
+        if (i > colors.Length - 1) {
+            i = 0;
+        }
+    }
+
+    public override void Teleport (Transform fromPortal, Transform toPortal, Vector3 pos, Quaternion rot) {
+        base.Teleport (fromPortal, toPortal, pos, rot);
+        rigidbody.velocity = toPortal.TransformVector (fromPortal.InverseTransformVector (rigidbody.velocity));
+        rigidbody.angularVelocity = toPortal.TransformVector (fromPortal.InverseTransformVector (rigidbody.angularVelocity));
+    }
+}
