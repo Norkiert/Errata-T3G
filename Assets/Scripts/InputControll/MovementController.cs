d595934cd6e0dc395a9db38ccca74f7345d0b266
Xardas221 <ziebamicha@gmail.com>
2021-12-07 18:38:53 +0100

    Skrypt poruszania c.d
    Drobne zmiany skryptu
    Dodanie opcji kucania

--- Assets/Scripts/InputControll/MovementController.cs
+++ Assets/Scripts/InputControll/MovementController.cs
@@ -1,60 +1,112 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class MovementController : MonoBehaviour
 {
-    private Rigidbody Rb;
-    private bool InSprint;
+    private Rigidbody rB;
+    private Transform model;
+    private bool inSprint;
     private bool isOnGround;
+    private bool isOnCrouch = false;
+    private float actualHeight=1.7f;
 
-    public float Speed;
-    public float SprintSpeed;
-    public float JumpForce;
-    public LayerMask GroundMask;
 
-    float HorizontalMove;
-    float VerticalMove;
+    [SerializeField] private float speed;
+    [SerializeField] private float sprintSpeed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float crouchSpeed;
+    [SerializeField] private LayerMask groundMask;
+
+    float horizontalMove;
+    float verticalMove;
     void Start()
     {
-        Rb = GetComponent<Rigidbody>();
+        rB = GetComponent<Rigidbody>();
+        model = GetComponent<Transform>();
     }
 
     void Update()
     {
-
-        isOnGround = Physics.CheckSphere(new Vector3(transform.position.x, transform.position.y - 1.7f, transform.position.z), 0.5f, GroundMask);
-
-        if (InSprint)
+        actualHeight = transform.localScale.y;
+        isOnGround = Physics.CheckSphere(new Vector3(transform.position.x, transform.position.y - actualHeight, transform.position.z), 0.5f, groundMask);
+        
+        if(isOnCrouch)
         {
-            HorizontalMove = Input.GetAxis("Horizontal") * SprintSpeed;
-            VerticalMove = Input.GetAxis("Vertical") * SprintSpeed;
+            horizontalMove = Input.GetAxis("Horizontal") * crouchSpeed;
+            verticalMove = Input.GetAxis("Vertical") * crouchSpeed;
+        }
+        else if (inSprint)
+        {
+            horizontalMove = Input.GetAxis("Horizontal") * sprintSpeed;
+            verticalMove = Input.GetAxis("Vertical") * sprintSpeed;
         }
         else
         {
-            HorizontalMove = Input.GetAxis("Horizontal") * Speed;
-            VerticalMove = Input.GetAxis("Vertical") * Speed;
+            horizontalMove = Input.GetAxis("Horizontal") * speed;
+            verticalMove = Input.GetAxis("Vertical") * speed;
         }
 
-        Vector3 MovePosition = transform.right * HorizontalMove + transform.forward * VerticalMove;
-        Vector3 NewMovePosition = new Vector3(MovePosition.x, Rb.velocity.y, MovePosition.z);
-        Rb.velocity = NewMovePosition;
+        Vector3 MovePosition = transform.right * horizontalMove + transform.forward * verticalMove;
+        Vector3 NewMovePosition = new Vector3(MovePosition.x, rB.velocity.y, MovePosition.z);
+        rB.velocity = NewMovePosition;
 
+        //sprint on/off
+        if (Input.GetKey(KeyCode.LeftShift) && isOnGround)
+        {
+            inSprint = true;
+        }
+        else
+        {
+            inSprint = false;
+        }
+
+        //Crouch controller
+        if(Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            if (isOnGround)
+            {
+                if (isOnCrouch)
+                {
+                   // actualHeight = 1.7f;
+                    model.DOScaleY(1.7f, 0.25f);
+                    isOnCrouch = false;
+                }
+                else
+                {
+                   // actualHeight = 1f;
+                    model.DOScaleY(1f, 0.3f);
+                    isOnCrouch = true;
+                }
+            }
+        }
+
+        //jumping
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (isOnGround)
             {
-                Rb.velocity = new Vector3(Rb.velocity.x, JumpForce, Rb.velocity.z);
+                if (Input.GetKey(KeyCode.LeftControl)&&isOnCrouch)
+                {
+                    rB.velocity = new Vector3(rB.velocity.x, jumpForce * 1.2f, rB.velocity.z);
+                    model.DOScaleY(1.7f, 0.01f);
+                    isOnCrouch = false;
+                }
+                else
+                {
+                    if (isOnCrouch)
+                    {
+                        rB.velocity = new Vector3(rB.velocity.x, jumpForce, rB.velocity.z);
+                        model.DOScaleY(1.7f, 0.01f);
+                        isOnCrouch = false;
+                    }
+                    else
+                    {
+                        rB.velocity = new Vector3(rB.velocity.x, jumpForce, rB.velocity.z);
+                    }
+                }
             }
-        }
-
-        if (Input.GetKey(KeyCode.LeftShift) && isOnGround)
-        {
-            InSprint = true;
-        }
-        else
-        {
-            InSprint = false;
         }
     }
 }
