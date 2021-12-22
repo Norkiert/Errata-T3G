c7f86bb04e9b8befd8114742a8041c749bd80a20
Hrober <bychrober@gmail.com>
2021-12-22 17:48:20 +0100

    Fixed player controller

--- Assets/Scripts/InputControll/MovementController.cs
+++ Assets/Scripts/InputControll/MovementController.cs
@@ -1,99 +1,188 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using DG.Tweening;
+using NaughtyAttributes;
+
 
 public class MovementController : MonoBehaviour
 {
-    private Rigidbody rB;
-    private Transform model;
-    private bool inSprint;
-    private bool isCrouching = false;
+    public enum Mods { None, Default, Sprint, Crouch }
 
-    [SerializeField] private float speed;
-    [SerializeField] private float sprintSpeed;
-    [SerializeField] private float jumpForce;
-    [SerializeField] private float crouchSpeed;
-    [SerializeField] private LayerMask groundMask;
+    [Header("Default")]
+    [SerializeField] [Min(1f)]
+    private float defaultSpeed;
 
-    void Start()
+    [SerializeField] [Min(0.5f)]
+    private float defaultHeight = 1.9f;
+
+    [SerializeField]
+    private LayerMask groundMask;
+
+    [SerializeField]
+    private bool canJump = true;
+
+    [SerializeField] [ShowIf(nameof(canJump))] [Min(1f)]
+    private float defaultJumpHeight;
+
+    [Header("Sprint")]
+    [SerializeField]
+    private bool canSprint = true;
+
+    [SerializeField] [ShowIf(nameof(canSprint))] [Min(1f)]
+    private float sprintSpeed;
+
+    [SerializeField] [ShowIf(EConditionOperator.And, nameof(canSprint), nameof(canJump))] [Min(1f)]
+    private float sprintJumpHeight;
+
+    [Header("Crouch")]
+    [SerializeField]
+    private bool canCrouch = true;
+
+    [SerializeField] [ShowIf(nameof(canCrouch))] [Min(1f)]
+    private float crouchSpeed;
+
+    [SerializeField] [ShowIf(EConditionOperator.And, nameof(canCrouch), nameof(canJump))] [Min(1f)]
+    private float crouchJumpHeight;
+
+    [SerializeField] [ShowIf(nameof(canCrouch))] [Min(0.5f)]
+    private float crouchHeight = 1.2f;
+
+
+    [Header("Inputs")]
+    [SerializeField] [ReadOnly] private bool inputJump = false;
+    [SerializeField] [ReadOnly] private bool inputSprint = false;
+    [SerializeField] [ReadOnly] private bool inputCrouch = false;
+    [SerializeField] [ReadOnly] private float inputHorizontalMove = 0f;
+    [SerializeField] [ReadOnly] private float inputVerticalMove = 0f;
+
+    [Header("States")]
+    [SerializeField] [ReadOnly] private bool isGrounded = false;
+    [SerializeField] [ReadOnly] private Mods currentMod = Mods.None;
+    [SerializeField] [ReadOnly] private float currentSpeed;
+    [SerializeField] [ReadOnly] private float currentJumpHeight;
+
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private Vector3 moveDirection;
+    private DG.Tweening.Tweener hightModifier;
+
+    private void Awake()
     {
-        rB = GetComponent<Rigidbody>();
-        model = GetComponent<Transform>();
+        controller = GetComponent<CharacterController>();
     }
 
-    void Update()
+    private void Start()
     {
-        float horizontalMove;
-        float verticalMove;
-        float currentHeight = transform.localScale.y;
-        bool isOnGround = Physics.CheckSphere(new Vector3(transform.position.x, transform.position.y - currentHeight, transform.position.z), 0.5f, groundMask);
+        SetMode(Mods.Default);
+    }
 
-        if(isCrouching)
+    private void Update()
+    {
+        Vector3 groundCheck = controller.bounds.center - (controller.bounds.extents.y - 0.1f) * Vector3.up;
+        isGrounded = Physics.CheckSphere(groundCheck, 0.3f, groundMask);
+
+        GetInput();
+        ChoseMode();
+        MovePlayer();
+    }
+
+    private void SetMode(Mods mod)
+    {
+        if (currentMod == mod)
+            return;
+
+        switch (mod)
         {
-            horizontalMove = Input.GetAxis("Horizontal") * crouchSpeed;
-            verticalMove = Input.GetAxis("Vertical") * crouchSpeed;
+            case Mods.Default:
+                currentSpeed = defaultSpeed;
+                currentJumpHeight = defaultJumpHeight;
+                SetHeight(defaultHeight, 0.3f);
+                break;
+            case Mods.Sprint:
+                currentSpeed = sprintSpeed;
+                currentJumpHeight = sprintJumpHeight;
+                SetHeight(defaultHeight, 0.3f);
+                break;
+            case Mods.Crouch:
+                currentSpeed = crouchSpeed;
+                currentJumpHeight = crouchJumpHeight;
+                SetHeight(crouchHeight, 0.5f);
+                break;
+            default:
+                currentSpeed = 0;
+                currentJumpHeight = 0;
+                SetHeight(defaultHeight);
+                break;
         }
-        else if (inSprint)
-        {
-            horizontalMove = Input.GetAxis("Horizontal") * sprintSpeed;
-            verticalMove = Input.GetAxis("Vertical") * sprintSpeed;
-        }
+
+        currentMod = mod;
+    }
+
+    private void ChoseMode()
+    {
+        if (canSprint && inputSprint)
+            SetMode(Mods.Sprint);
+        else if (canCrouch && inputCrouch)
+            SetMode(Mods.Crouch);
         else
-        {
-            horizontalMove = Input.GetAxis("Horizontal") * speed;
-            verticalMove = Input.GetAxis("Vertical") * speed;
-        }
+            SetMode(Mods.Default);
+    }
 
-        Vector3 MovePosition = transform.right * horizontalMove + transform.forward * verticalMove;
-        Vector3 NewMovePosition = new Vector3(MovePosition.x, rB.velocity.y, MovePosition.z);
-        rB.velocity = NewMovePosition;
+    private void SetHeight(float targetHeight, float speed)
+    {
+        hightModifier?.Kill();
+        float time = Math.Abs(controller.height - targetHeight) * speed;
+        hightModifier = DOVirtual.Float(controller.height, targetHeight, time, v => SetHeight(v, false));
+    }
+    private void SetHeight(float targetHeight, bool kill = true)
+    {
+        if(kill)
+            hightModifier?.Kill();
+
+        if (isGrounded)
+            controller.Move(transform.up * (targetHeight - controller.height));
+
+        controller.height = targetHeight;
+    }
+
+
+    private void GetInput()
+    {
+        // movement
+        inputHorizontalMove = Input.GetAxisRaw("Horizontal");
+        inputVerticalMove = Input.GetAxisRaw("Vertical");
 
         //sprint on/off
-        if (Input.GetKey(KeyCode.LeftShift) && isOnGround)
-        {
-            inSprint = true;
-        }
-        else
-        {
-            inSprint = false;
-        }
+        inputSprint = Input.GetKey(KeyCode.LeftShift);
 
         //Crouching controller
-        if(Input.GetKeyDown(KeyCode.LeftControl))
-        {
-            if (isOnGround)
-            {
-                if (isCrouching)
-                {
-                    isCrouching = false;
-                    model.DOScaleY(1.7f, 0.25f);
-                }
-                else
-                {
-                    isCrouching = true;
-                    model.DOScaleY(1f, 0.3f);
-                }
-            }
-        }
+        inputCrouch = Input.GetKey(KeyCode.LeftControl);
 
         //jumping
-        if (Input.GetKeyDown(KeyCode.Space))
+        inputJump = Input.GetKey(KeyCode.Space);
+    }
+
+    private void MovePlayer()
+    {
+        Vector3 newMoveDirection = transform.right * inputHorizontalMove + transform.forward * inputVerticalMove;
+        float smothnes = (inputHorizontalMove > 0 || inputVerticalMove > 0 ? 10f : 100f) * Time.deltaTime;
+        moveDirection = Vector3.Lerp(moveDirection, newMoveDirection, smothnes);
+        controller.Move(moveDirection.normalized * currentSpeed * Time.deltaTime);
+
+        if (isGrounded)
         {
-            if (isOnGround)
-            {
-                    if (isCrouching)
-                    {
-                        rB.velocity = new Vector3(rB.velocity.x, jumpForce, rB.velocity.z);
-                        model.DOScaleY(1.7f, 0.01f);
-                        isCrouching = false;
-                    }
-                    else
-                    {
-                        rB.velocity = new Vector3(rB.velocity.x, jumpForce, rB.velocity.z);
-                    }
-            }
+            if (velocity.y < 0)
+                velocity.y = -2f;
+
+            if (canJump && inputJump && velocity.y < 1f)
+                velocity.y = Mathf.Sqrt(currentJumpHeight * -2f * Physics.gravity.y);
         }
+
+        velocity += Physics.gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
 
