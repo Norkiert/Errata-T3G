4abbec1a79ad2aa9b4eeb43d7e4543431d54970e
Hrober <bychrober@gmail.com>
2021-12-30 16:39:59 +0100

    Fixes in lifatable and cables

--- Assets/Scripts/Interactions/Liftable.cs
+++ Assets/Scripts/Interactions/Liftable.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,25 +9,42 @@ public class Liftable : Interactable
     [field: SerializeField] public Vector3 LiftDirectionOffset { get; private set; } = Vector3.zero;
 
     public Rigidbody Rigidbody { get; protected set; }
-    private int defaultLayer;
+    private readonly List<(GameObject, int)> defaultLayers = new List<(GameObject obj, int defaultLayer)>();
+
+    private bool isLift = false;
 
     protected override void Awake()
     {
         base.Awake();
         Rigidbody = GetComponent<Rigidbody>();
-        defaultLayer = gameObject.layer;
     }
 
     public void PickUp(int layer)
     {
-        gameObject.layer = layer;
+        if (isLift)
+            return;
+
+        // save layers
+        defaultLayers.Clear();
+        foreach (Collider col in gameObject.GetComponentsInChildren<Collider>())
+            defaultLayers.Add((col.gameObject, col.gameObject.layer));
+
+        // set
         Rigidbody.useGravity = false;
         Rigidbody.interpolation = RigidbodyInterpolation.Interpolate;
+        foreach ((GameObject obj, int defaultLayer) item in defaultLayers)
+            item.obj.layer = layer;
+
+       isLift = true;
     }
     public void Drop()
     {
-        gameObject.layer = defaultLayer;
+        if (!isLift)
+            return;
+
         Rigidbody.useGravity = true;
         Rigidbody.interpolation = RigidbodyInterpolation.None;
+        foreach ((GameObject obj, int defaultLayer) item in defaultLayers)
+            item.obj.layer = item.defaultLayer;
     }
 }
