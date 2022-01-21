bd0ee7fdd68afd5b80cf425b93affe55731145f8
Hrober <bychrober@gmail.com>
2022-01-21 15:41:06 +0100

    Kostki w Hubie

---
+++ Assets/Scripts/GameManagment/HubSelectDimension.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class HubSelectDimension : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> cubes = new List<GameObject>();
+    [SerializeField] private List<Connector> cubesConnect = new List<Connector>();
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
