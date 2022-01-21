0cff3bbe03c4588a5c1b57da0d147ba7abaa007c
Hrober <bychrober@gmail.com>
2022-01-21 22:25:18 +0100

    Animacja ruchu kostek w hubie

--- Assets/Scripts/GameManagment/HubSelectDimension.cs
+++ Assets/Scripts/GameManagment/HubSelectDimension.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using NaughtyAttributes;
+using DG.Tweening;
 
 namespace GameManagment
 {
@@ -14,14 +15,23 @@ namespace GameManagment
 
         [SerializeField] private List<ConnectCube> cubesConnect = new List<ConnectCube>();
 
+        [Header("Movement")]
+        [SerializeField] private float minCubeYPos = -0.3f;
+        [SerializeField] private float maxCubeYPos = 1f;
+        [SerializeField, Min(0)] private int conCubeImpact = 2;
+        [SerializeField, Min(0)] private float conMoveTime = 4;
+
         private const int numberOfCubesInRow = 16;
         private const int numberOfCubesParentsInRow = 2;
         private const float cubeSzie = 0.6f;
-        private Transform[,] cubes;
+        
+        private Cube[,] cubes;
 
         private void Start()
         {
-            SetCunes();
+            SetCubes();
+
+            UpdateAllCubesMovement();
 
             StartCoroutine(CheckConnections());
         }
@@ -41,12 +51,16 @@ namespace GameManagment
                     if (connectCube.Connector.IsConnected)
                     {
                         selectedDimension = connectCube.Dimension;
+                        UpdateCubesMovement(connectCube.X, connectCube.Y, false);
                         break;
                     }
                 }
 
                 if (DimensionManager.LoadedDimension != selectedDimension)
                 {
+                    if (selectedDimension == DimensionManager.Dimension.Main)
+                        UpdateAllCubesMovement();
+
                     DimensionManager.LoadDimension(selectedDimension);
                     while (DimensionManager.LoadedDimension != selectedDimension)
                         yield return null;
@@ -54,7 +68,7 @@ namespace GameManagment
             }
         }
 
-        private void SetCunes()
+        private void SetCubes()
         {
             if (!cubeParentBL || !cubeParentBR || !cubeParentTL || !cubeParentTR)
             {
@@ -63,7 +77,7 @@ namespace GameManagment
             }
 
             int width = numberOfCubesInRow * numberOfCubesParentsInRow;
-            cubes = new Transform[width, width];
+            cubes = new Cube[width, width];
 
             SetInArry(cubeParentBR, 0, 0);
             SetInArry(cubeParentBL, numberOfCubesInRow, 0);
@@ -79,13 +93,115 @@ namespace GameManagment
                     Transform cube = parent.GetChild(i);
                     int x = Mathf.RoundToInt(cube.localPosition.x * -tcubeSzie) / 100;
                     int y = Mathf.RoundToInt(cube.localPosition.z * tcubeSzie) / 100;
-                    cubes[x + offsetX, y + offsetY] = cube;
+                    cubes[x + offsetX, y + offsetY] = new Cube(cube);
 
                     for (int c = 0; c < cubesConnect.Count; c++)
                     {
                         if (cube == cubesConnect[c].Transform)
                             cubesConnect[c].Init(x + offsetX, y + offsetY);
                     }
+                }
+            }
+        }
+
+        private void UpdateAllCubesMovement()
+        {
+            foreach (ConnectCube connectCube in cubesConnect)
+            {
+                if (!connectCube.Transform)
+                    continue;
+
+                UpdateCubesMovement(connectCube.X, connectCube.Y, true);
+            }
+        }
+
+        private void UpdateCubesMovement(int cx, int cy, bool loop)
+        {
+            Dictionary<Cube, float> moves = new Dictionary<Cube, float>();
+
+            float maxPos = conCubeImpact * 1.4f;
+            for (int y = -conCubeImpact; y < conCubeImpact; y++)
+            {
+                int fy = cy + y;
+                if (fy < 0 || fy >= numberOfCubesInRow * numberOfCubesParentsInRow)
+                    continue;
+
+                for (int x = -conCubeImpact; x < conCubeImpact; x++)
+                {
+                    int fx = cx + x;
+                    if (fx < 0 || fx >= numberOfCubesInRow * numberOfCubesParentsInRow)
+                        continue;
+
+                    float power = Mathf.Max(0, maxPos - new Vector2(x, y).magnitude);
+                    float targetY = Map(power, 0, maxPos, minCubeYPos, maxCubeYPos);
+
+                    if (targetY <= 0)
+                        continue;
+
+                    if (moves.TryGetValue(cubes[fx, fy], out float actTargetY))
+                    {
+                        if (actTargetY < targetY)
+                            moves[cubes[fx, fy]] = targetY;
+                    }
+                    else
+                        moves.Add(cubes[fx, fy], targetY);
+                }
+            }
+
+            foreach (var item in moves)
+                item.Key.Move(minCubeYPos, item.Value, conMoveTime, loop);
+        }
+
+        private float Map(float x, float in_min, float in_max, float out_min, float out_max) => (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+
+        private class Cube
+        {
+            [field: SerializeField, Required]
+            public Transform Transform { get; private set; }
+
+
+            private Sequence sequence;
+
+            public Cube(Transform transform)
+            {
+                Transform = transform;
+            }
+
+            public void CancelMove()
+            {
+                sequence.Kill();
+                Transform.DOKill();
+            }
+            public void Move(float minY, float maxY, float moveTime, bool loop)
+            {
+                CancelMove();
+
+                moveTime *= Random.Range(0.5f, 1.5f);
+
+                if (loop)
+                {
+                    if (Mathf.Abs(Transform.localPosition.y - minY) < Mathf.Abs(Transform.localPosition.y - maxY))
+                    {
+                        sequence = DOTween.Sequence()
+                            .SetLoops(-1)
+                            .Append(Transform.DOLocalMoveY(maxY, moveTime))
+                            .AppendInterval(moveTime / 2f)
+                            .Append(Transform.DOLocalMoveY(minY, moveTime))
+                            ;
+                    }
+                    else
+                    {
+                        sequence = DOTween.Sequence()
+                            .SetLoops(-1)
+                            .Append(Transform.DOLocalMoveY(minY, moveTime))
+                            .Append(Transform.DOLocalMoveY(maxY, moveTime))
+                            .AppendInterval(moveTime / 2f)
+                            ;
+                    }
+                }
+                else
+                {
+                    Transform.DOLocalMoveY(maxY, moveTime);
                 }
             }
         }
