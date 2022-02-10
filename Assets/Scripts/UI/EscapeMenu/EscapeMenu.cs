ae4b5f8cbd5a912d7f7214641945ce4245c2f973
Xardas221 <ziebamicha@gmail.com>
2022-02-10 19:03:04 +0100

    wstępne funkcjonalności menu pauzy

--- Assets/Scripts/UI/EscapeMenu/EscapeMenu.cs
+++ Assets/Scripts/UI/EscapeMenu/EscapeMenu.cs
@@ -1,15 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using GameManagment;
 
 public class EscapeMenu : MonoBehaviour
 {
     [SerializeField] private Canvas escMenu;
+    [SerializeField] private GameObject settingsPanel;
+
+    [SerializeField] private Button resumeGameButton;
+    [SerializeField] private Button backToLobbyButton;
+    [SerializeField] private Button loadGameButton;
+    [SerializeField] private Button settingsButton;
+    [SerializeField] private Button backToMenuButton;
+
 
     private void Start()
     {
+        resumeGameButton.onClick.AddListener(ResumeGame);
+        backToLobbyButton.onClick.AddListener(BackToLobby);
+        loadGameButton.onClick.AddListener(LoadGame);
+        settingsButton.onClick.AddListener(OpenSettings);
+        backToMenuButton.onClick.AddListener(BackToMenu);
         escMenu.enabled = false;
+        settingsPanel.SetActive(false);
     }
 
     void Update()
@@ -18,6 +33,7 @@ public class EscapeMenu : MonoBehaviour
         {
             if (GameManager.IsGamePaused)
             {
+                settingsPanel.SetActive(false);
                 GameManager.ResumeGame();
                 escMenu.enabled = false;
             }
@@ -28,5 +44,28 @@ public class EscapeMenu : MonoBehaviour
             }
         }
     }
-    //Cursor.SetCursor(default, Vector2.zero, CursorMode.ForceSoftware);
+    #region -Buttons functionality-
+    private void ResumeGame()
+    {
+        settingsPanel.SetActive(false);
+        GameManager.ResumeGame();
+        escMenu.enabled = false;
+    }
+    private void BackToLobby()
+    {
+        Debug.Log("ESC MENU: back to lobby");
+    }
+    void LoadGame()
+    {
+        Debug.Log("ESC MENU: load game");
+    }
+    private void OpenSettings()
+    {
+        settingsPanel.SetActive(true);
+    }
+    private void BackToMenu()
+    {
+        Debug.Log("ESC MENU: Back to main menu");
+    }
+    #endregion
 }
