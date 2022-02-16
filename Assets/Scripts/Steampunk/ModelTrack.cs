5b35a0fd0957a388176728647e566ab0cfc08524
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2022-02-16 18:08:13 +0100

    Added modelLength, modelHeigth and modelWidth to ModelTrack(based on StraigthTrack model).)

--- Assets/Scripts/Steampunk/ModelTrack.cs
+++ Assets/Scripts/Steampunk/ModelTrack.cs
@@ -10,4 +10,7 @@ public class ModelTrack
     public const float trackMapCellSize = length;
     public const float scale = 5f;
     public const string prefabPath = "Assets/Art/Dimensions/Steampunk/Prefabs/StraightTrack.prefab";
+    public const float modelLength = 0.2705f * 2;
+    public const float modelHeight = 0.05333012f * 2;
+    public const float modelWidth = 0.1085f * 2;
 }
