46d609a0133216517871d5993d97e5f36b0bef30
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2022-03-17 20:59:46 +0100

    Made CurvedTrack an abstract class.

--- Assets/Scripts/Steampunk/CurvedTrack.cs
+++ Assets/Scripts/Steampunk/CurvedTrack.cs
@@ -2,20 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CurvedTrack : BasicTrack
+public abstract class CurvedTrack : BasicTrack
 {
     public new const float length = (ModelTrack.length + ModelTrack.width) / 2;
     public new const float height = ModelTrack.height;
     public new const float width = length;
-    //public const float radius = 
+    public new const string prefabPath = "";
+    public const float radius = length - ModelTrack.width / 2;
+    public const float realLength = 0.5f * Mathf.PI * radius;
+    
 
     public new void Awake()
     {
         base.Awake();
     }
-    public override void MoveBall(BallBehavior ball)
+    public override void RotateRight()
     {
-        
+        transform.RotateAround(GetPosition(), Vector3.up, 90);
+    }
+    public override void RotateLeft()
+    {
+        transform.RotateAround(GetPosition(), Vector3.up, -90);
     }
     public override void AlignTo(TrackConnectionInfo tci)
     {
@@ -23,9 +30,34 @@ public class CurvedTrack : BasicTrack
         {
             Debug.LogError("Cannot align to track with different parent!");
         }
+        else if (tci.track.NeighborTracks[(int)tci.level, (int)tci.position] != this && tci.track.NeighborTracks[(int)tci.level, (int)tci.position])
+        {
+            Debug.LogError($"Cannot align to track at that position: {{{(int)tci.level}}}, {{{(int)tci.position}}}");
+        }
         else
         {
-
+            transform.position = new Vector3(tci.track.transform.position.x, tci.track.transform.position.y, tci.track.transform.position.z);
+            transform.localPosition = new Vector3(tci.track.transform.localPosition.x, tci.track.transform.localPosition.y, tci.track.transform.localPosition.z);
+            switch (tci.position)
+            {
+                case NeighborPosition.Xplus:
+                    transform.localPosition += length * Vector3.right;
+                    break;
+                case NeighborPosition.Zminus:
+                    transform.localPosition += length * Vector3.back;
+                    break;
+                case NeighborPosition.Xminus:
+                    transform.localPosition += length * Vector3.left;
+                    break;
+                case NeighborPosition.Zplus:
+                    transform.localPosition += length * Vector3.forward;
+                    break;
+            }
+            switch (tci.level)
+            {
+                case NeighborLevel.same:
+                    break;
+            }
         }
     }
 }
