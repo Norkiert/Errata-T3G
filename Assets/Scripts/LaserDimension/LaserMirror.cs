051e1f7e4649cb58639626f0665ab845ab67f29b
Xardas221 <ziebamicha@gmail.com>
2022-03-25 20:11:23 +0100

    Dodanie możliwości obracania lustra o konkretną ilość stopni

--- Assets/Scripts/LaserDimension/LaserMirror.cs
+++ Assets/Scripts/LaserDimension/LaserMirror.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NaughtyAttributes;
 using DG.Tweening;
 public class LaserMirror : Interactable
 {
+    [Header("Rotation types")]
     [SerializeField] private bool rotateHorizontal = true;
     [SerializeField] private bool rotateVertical = false;
+    [Header("Rotation options")]
     [SerializeField] private float rotationSpeed = 20;
+    [SerializeField] private bool rotateBySteps = false;
+    [SerializeField, ShowIf(nameof(rotateBySteps))] float degreesPerStep = 30f;
+    [SerializeField, ShowIf(nameof(rotateBySteps))] float mouseMoveDelayOfStepRotation = 8f;
 
     private PlayerInteractions player;
     private PlayerController playerController;
@@ -42,12 +48,53 @@ public class LaserMirror : Interactable
     }
     private IEnumerator RotateMirror()
     {
+        float stepCheckHorizontal = 0f;
+        float stepCheckVertical = 0f;
         float reverseRotating = 1f;
         while(!playerController.enabled)
         {
-            reverseRotating = (transform.position.x>player.transform.position.x)?-1f:1f;
-            yield return null;
-            transform.Rotate(new Vector3(0, rotateHorizontal?Input.GetAxis("Mouse X")*reverseRotating:0, rotateVertical?Input.GetAxis("Mouse Y"):0) * rotationSpeed * Time.deltaTime);
+            if (rotateBySteps)
+            {
+                reverseRotating = (transform.position.x > player.transform.position.x) ? -1f : 1f;
+                if (rotateHorizontal)
+                {
+                    if(Input.GetAxis("Mouse X")>0&&stepCheckHorizontal<0)
+                        stepCheckHorizontal = 0f + Input.GetAxis("Mouse X");
+                    if (Input.GetAxis("Mouse X") < 0 && stepCheckHorizontal > 0)
+                        stepCheckHorizontal = 0f - Input.GetAxis("Mouse X");
+
+                    if (stepCheckHorizontal > mouseMoveDelayOfStepRotation || stepCheckHorizontal < -mouseMoveDelayOfStepRotation)
+                    {
+                        transform.Rotate(new Vector3(0, (degreesPerStep * (stepCheckHorizontal < 0 ? -1 : 1)*reverseRotating), 0));
+                        stepCheckHorizontal = 0f;
+                    }
+                    else
+                        stepCheckHorizontal += Input.GetAxis("Mouse X");
+                }
+                if(rotateVertical)
+                {
+                    if (Input.GetAxis("Mouse Y") > 0 && stepCheckVertical < 0)
+                        stepCheckVertical = 0f + Input.GetAxis("Mouse Y");
+                    if (Input.GetAxis("Mouse Y") < 0 && stepCheckVertical > 0)
+                        stepCheckVertical = 0f - Input.GetAxis("Mouse Y");
+
+                    if (stepCheckVertical > mouseMoveDelayOfStepRotation || stepCheckVertical < -mouseMoveDelayOfStepRotation)
+                    {
+                        transform.Rotate(new Vector3(0, 0, (degreesPerStep * (stepCheckVertical < 0 ? -1 : 1) * reverseRotating)));
+                        stepCheckVertical = 0f;
+                    }
+                    else
+                        stepCheckVertical += Input.GetAxis("Mouse Y");
+                }
+               
+                yield return null;
+            }
+            else
+            {
+                reverseRotating = (transform.position.x>player.transform.position.x)?-1f:1f;
+                yield return null;
+                transform.Rotate(new Vector3(0, rotateHorizontal?Input.GetAxis("Mouse X")*reverseRotating:0, rotateVertical?Input.GetAxis("Mouse Y"):0) * rotationSpeed * Time.deltaTime);
+            }
         }
     }
 }
