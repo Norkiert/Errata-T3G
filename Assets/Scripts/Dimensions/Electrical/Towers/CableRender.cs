e0ebe96298ed1a4da244e6795cb6cfeec8be61a6
Hrober <bychrober@gmail.com>
2022-04-13 23:05:24 +0200

    Improved CableRender script

--- Assets/Scripts/Dimensions/Electrical/Towers/CableRender.cs
+++ Assets/Scripts/Dimensions/Electrical/Towers/CableRender.cs
@@ -5,25 +5,62 @@ using NaughtyAttributes;
 
 public class CableRender : MonoBehaviour
 {
-    [SerializeField] bool drawLines = false;
-    [SerializeField, ShowIf(nameof(drawLines))] private GameObject targetTower;
+    [SerializeField] private GameObject targetTower;
+
+    [SerializeField, Min(0)] private int additionalPoints = 4;
+    [SerializeField, Min(0)] private float dangle = 0.4f;
+    [SerializeField, Range(0,1)] private float dangleScale = 6f;
+    [SerializeField] private Vector3 dangleDirection = Vector3.down;
+
+    [Button("Set lines")]
+    private void SetLines()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            string name = $"Target{i + 1}";
+            Transform thisPoint = transform.Find(name);
+            Transform targetPoint = targetTower.transform.Find(name);
+
+            if (thisPoint == null || targetPoint == null)
+            {
+                Debug.LogWarning($"Dont found \"{name}\" point");
+                continue;
+            }
+
+            LineRenderer line = thisPoint.GetComponent<LineRenderer>();
+
+            Vector3 startPos = thisPoint.position;
+            Vector3 endPos = targetPoint.position;
+
+            Vector3[] points = new Vector3[additionalPoints + 2];
+            points[0] = startPos;
+            points[additionalPoints + 1] = endPos;
+
+            Vector3 tPos = startPos;
+            Vector3 xOffset = (endPos - startPos) / (float)(additionalPoints + 1);
+            for (int ip = 1; ip <= additionalPoints; ip++)
+            {
+                tPos += xOffset;
+                points[ip] = tPos;
+            }
+
+            float tDangle = 0;
+            for (int ip = 1; ip <= additionalPoints / 2; ip++)
+            {
+                tDangle += (2 * dangle - tDangle) * dangleScale * dangle / (float)additionalPoints;
+                points[ip] += dangleDirection * tDangle;
+                points[additionalPoints - ip + 1] += dangleDirection * tDangle;
+            }
+            if (additionalPoints % 2 == 1)
+                points[additionalPoints / 2 + 1] += dangleDirection * tDangle;
+
+            line.positionCount = points.Length;
+            line.SetPositions(points);
+        }
+    }
 
     private void Start()
     {
-        if(drawLines)
-        {
-            GameObject[] targets = new GameObject[6];
-            GameObject[] towerPoints = new GameObject[6];
-            LineRenderer[] lines = new LineRenderer[6];
-            for (int i = 1; i <= 6; i++)
-            {
-                targets[i - 1] = targetTower.transform.Find(("Target" + i).ToString()).gameObject;
-                towerPoints[i - 1] = transform.Find(("Target" + i).ToString()).gameObject;
-                lines[i - 1] = towerPoints[i - 1].GetComponent<LineRenderer>();
-
-                lines[i - 1].SetPosition(0, towerPoints[i - 1].transform.position);
-                lines[i - 1].SetPosition(1, targets[i - 1].transform.position);
-            }
-        }
+        SetLines();
     }
 }
