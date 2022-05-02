f27ea566fa01731af2c3a7bebb8d19049e556c5f
bagnohash <klocseb15@gmail.com>
2022-05-02 13:31:42 +0200

    Reset save game, all button

--- Assets/Scripts/Saving/HroberPrefsReset.cs
+++ Assets/Scripts/Saving/HroberPrefsReset.cs
@@ -2,16 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using NaughtyAttributes;
+using System.IO;
 
 public class HroberPrefsReset : MonoBehaviour
 {
     [Button]
-    public void Reset()
+    public void ResetHroberPrefs()
     {
         HroberPrefs.Load();
         HroberPrefs.data.bools.Clear();
         HroberPrefs.data.ints.Clear();
         HroberPrefs.data.floats.Clear();
         HroberPrefs.Save();
+    }
+
+    [Button]
+    public void ResetSaveGame()
+    {
+        string path = Application.persistentDataPath + "/errata.json";
+        if (File.Exists(path))
+        {
+            SaveData save = new SaveData();
+            string json = JsonUtility.ToJson(save);
+            File.WriteAllText(path, json);
+        }
+    }
+
+    [Button]
+    public void ResetAll()
+    {
+        ResetSaveGame();
+        ResetHroberPrefs();
     }
 }
