32dd074bfad0be91237e001025a12a77e04dc4ad
Xardas221 <ziebamicha@gmail.com>
2022-05-03 14:00:44 +0200

    Added script for Q2 electrical scene

---
+++ Assets/Scripts/Dimensions/Electrical/Quests/Q2/Q2SillDisable.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+public class Q2SillDisable : MonoBehaviour
+{
+    [Header("Connectors to check")]
+    [SerializeField] private Connector con1;
+    [SerializeField] private Connector con2;
+
+    [Header("Other")]
+    [SerializeField] private float checkDelay = 2f;
+    [SerializeField] private BoxCollider sill;
+    [SerializeField] private ParticleSystem sparks;
+
+    void Start()
+    {
+        checkConnections = CheckConnections();
+        StartCoroutine(checkConnections);
+    }
+
+    private IEnumerator checkConnections;
+
+    private IEnumerator CheckConnections()
+    {
+        while (!(con1.IsConnected && con2.IsConnected))
+        {
+            yield return new WaitForSeconds(checkDelay);
+        }
+        TurnOffSill();
+    }
+    private IEnumerator checkDisconection;
+    private IEnumerator CheckDisconection()
+    {
+        while (con1.IsConnected && con2.IsConnected)
+        {
+            yield return new WaitForSeconds(checkDelay);
+        }
+        TurnOnSill();
+    }
+    private void TurnOffSill()
+    {
+        if (checkConnections != null)
+            StopCoroutine(checkConnections);
+        sill.enabled = false;
+        sparks.Stop();
+        checkDisconection = CheckDisconection();
+        StartCoroutine(checkDisconection);
+    }
+    private void TurnOnSill()
+    {
+        if (checkDisconection != null)
+            StopCoroutine(checkDisconection);
+        checkConnections = CheckConnections();
+        StartCoroutine(checkConnections);
+        sill.enabled = true;
+        sparks.Play();
+    }
+}
+
--- Assets/Scripts/Dimensions/Electrical/Quests/Q2/Q2Switch.cs
+++ Assets/Scripts/Dimensions/Electrical/Quests/Q2/Q2Switch.cs
@@ -2,9 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using NaughtyAttributes;
+using Audio;
 
 public class Q2Switch : Interactable
 {
+    [SerializeField, Required] private ParticleSystem switchParticles;
+    [SerializeField, Required] private Connector con1;
+    [SerializeField, Required] private Connector con2;
+    [SerializeField, Required] private Connector con3;
+    [SerializeField, Required] private Connector con4;
+    [SerializeField, Required] private Connector con5;
+    [SerializeField, Required] private Connector con6;
+    [SerializeField, Required] private Q2Capsule capsuleConnector;
+    [SerializeField] private AudioClipSO invalidSound;
+
+    private bool done = false;
     public override void Select()
     {
         base.Select();
@@ -29,9 +42,28 @@ public class Q2Switch : Interactable
     }
     private void TrySwitchSwitchDown()
     {
-        transform.DOLocalRotate(new Vector3(0, 0, 180), 1f);
+        if(con1.IsConnected&& con2.IsConnected && con3.IsConnected && con4.IsConnected && con5.IsConnected && con6.IsConnected && !done)
+        {
+            done = true;
+            transform.DOLocalRotate(new Vector3(0, 0, 180), 1f);
+            capsuleConnector.SLideCapsuleDown();
+        }
+        else
+        {
+            if(!done)
+                StartCoroutine(RotateSwitch());
+        }    
     }
 
+    private IEnumerator RotateSwitch()
+    {
+        transform.DOLocalRotate(new Vector3(0, 0, 30), 0.2f);
+        yield return new WaitForSeconds(0.1f);
+        AudioManager.PlaySFX(invalidSound, transform.position);
+        switchParticles.Play();
+        yield return new WaitForSeconds(Time.deltaTime);
+        transform.DOLocalRotate(new Vector3(0, 0, 0), 0.2f);
+    }
 
 
 }
