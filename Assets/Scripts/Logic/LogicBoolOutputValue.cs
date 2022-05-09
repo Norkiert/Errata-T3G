f77f15c665bfc9b3f60009bc5edc033232924ab4
Hrober <bychrober@gmail.com>
2022-05-09 18:05:23 +0200

    Add LogicBoolOutputValue script

---
+++ Assets/Scripts/Logic/LogicBoolOutputValue.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Logic
+{
+    public class LogicBoolOutputValue : LogicBoolOutput
+    {
+        public override bool LogicValue => value;
+
+        private bool value = false;
+
+        public void SetValue(bool value) => this.value = value;
+    }
+}
