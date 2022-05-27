eeb6dcdb0a6c1e0da722af06e80e0c910c3e0402
Hrober <bychrober@gmail.com>
2022-05-27 21:35:28 +0200

    Dont play sound on not interactable buttons

--- Assets/Scripts/UI/OnHoverOnClick.cs
+++ Assets/Scripts/UI/OnHoverOnClick.cs
@@ -2,19 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Audio;
+using UnityEngine.UI;
 
 public class OnHoverOnClick : MonoBehaviour
 {
     public AudioClipSO audioOnHover;
     public AudioClipSO audioOnClick;
 
+    private Button button;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+    }
+
     public void onClick()
     {
-        AudioManager.PlaySFX(audioOnClick);
+        if (button.interactable)
+            AudioManager.PlaySFX(audioOnClick);
     }
 
     public void onHover()
     {
-        AudioManager.PlaySFX(audioOnHover);
+        if (button.interactable)
+            AudioManager.PlaySFX(audioOnHover);
     }
 }
