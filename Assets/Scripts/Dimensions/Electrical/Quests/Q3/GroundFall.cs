808940e3e31a81adf4bfed2d0c3848acb869dc86
Xardas221 <85582296+Xardas221@users.noreply.github.com>
2022-05-29 19:14:13 +0200

    Electrical scene update/fix and Q3 cable fixed

--- Assets/Scripts/Dimensions/Electrical/Quests/Q3/GroundFall.cs
+++ Assets/Scripts/Dimensions/Electrical/Quests/Q3/GroundFall.cs
@@ -5,14 +5,8 @@ using UnityEngine;
 public class GroundFall : MonoBehaviour
 {
     [SerializeField] private Connector cable;
-    //[SerializeField] private Connector cable2;
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("bul");
-        if (other.gameObject.TryGetComponent<PlayerController>(out _))
-        {
             cable.Disconnect(false);
-            //cable2.Disconnect(false);
-        }
     }
 }
